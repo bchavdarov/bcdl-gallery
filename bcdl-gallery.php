22f206598cb5cca1b17ba7dd2c7ad6df93608817
bchavdarov <spo@abv.bg>
2020-06-21 13:10:58 +0300

    main file

---
+++ bcdl-gallery.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       BCDLab Gallery
+ * Plugin URI:        https://bchavdarov.github.io/bcdlab/
+ * Description:       BCDL Gallery based on Bootstrap
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Boncho Chavdarov
+ * Author URI:        https://bchavdarov.github.io/bcdlab/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       bcdl-gallery
+ * Domain Path:       /languages
+ */
+
